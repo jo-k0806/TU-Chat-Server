abfe3112167f0020f5ddc6ad2ec562ad0850c2da
tuj04825 <tuj04825@temple.edu>
2022-09-21 11:24:07 -0400

    entering name
    allows user to enter a user name

--- Server.java
+++ Server.java
@@ -1,14 +1,19 @@
 import java.io.IOException;
 import java.net.*;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
+        System.out.println("Enter a name: ");
+        Scanner scanner=new Scanner(System.in);
+        String user = scanner.next();
+
         int portNumber = 6000;
         try{
             ServerSocket serverSocket = new ServerSocket(portNumber);
-            System.out.println("Joined port: " + portNumber);
+            System.out.println(user + " joined port: " + portNumber);
         } catch (IOException e){
-            System.err.println("Could not listen on port: " + portNumber);
+            System.err.println(user + " could not listen on port: " + portNumber);
             System.exit(1);
         }
     }
--- out/production/TU-Chat-Server/README.md
+++ out/production/TU-Chat-Server/README.md
@@ -1,2 +1,5 @@
 # TU-Chat-Server
 Chat server using Java
+
+How to Compile and Run
+- Use a java compiler or IDE
